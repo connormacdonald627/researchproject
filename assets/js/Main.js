48a74a0db3bcc41ffcedc09c78af0a6377932d25
BracketProto <connormacdonald627@gmail.com>
2026-02-25 18:28:53 +0000

    data stream direction

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -1,15 +1,27 @@
 import Header from "../components/header/Header.js";
 import { CreateChart } from "./CreateChart.js";
 import Carousel from "../components/carousel/Carousel.js";
-import Bandwidth_TCP from "../../data/bandwidth/tcp-summary.json" with { type: "json" };
-import Bandwidth_UDP from "../../data/bandwidth/udp-summary.json" with { type: "json" };
 
-Bandwidth_TCP.sort((a, b) => {
+//Bandwidth
+import BandwidthTCP from "../../data/bandwidth/tcp-summary.json" with { type: "json" };
+import BandwidthUDP from "../../data/bandwidth/udp-summary.json" with { type: "json" };
+
+//Upstream
+import UpstreamTCP from "../../data/streamdirection/upstream/tcp/upstream-summary.json" with { type: "json" };;
+import UpstreamUDP from "../../data/streamdirection/upstream/udp/upstream-summary.json" with { type: "json" };;
+
+//Downstream
+import DownstreamTCP from "../../data/streamdirection/downstream/tcp/downstream-summary.json" with { type: "json" };;
+import DownstreamUDP from "../../data/streamdirection/downstream/udp/downstream-summary.json" with { type: "json" };;
+
+
+//Bandwidth Charts
+BandwidthTCP.sort((a, b) => {
   const rateA = parseFloat(a.File.match(/rate(\d+\.?\d*)/)[1]);
   const rateB = parseFloat(b.File.match(/rate(\d+\.?\d*)/)[1]);
   return rateA - rateB;
 });
-Bandwidth_UDP.sort((a, b) => {
+BandwidthUDP.sort((a, b) => {
   const RateA = parseFloat(a.File.match(/rate(\d+\.?\d*)/)[1]);
   const RateB = parseFloat(b.File.match(/rate(\d+\.?\d*)/)[1]);
   return RateA - RateB;
@@ -17,7 +29,7 @@ Bandwidth_UDP.sort((a, b) => {
 
 const BandwidthTCPData = {
   label: "TCP Throughput",
-  data: Bandwidth_TCP.map((entry) => Number(entry.Analysis.ThroughputKbps)),
+  data: BandwidthTCP.map((entry) => Number(entry.Analysis.ThroughputKbps)),
   backgroundColor: "cyan",
   borderColor: "cyan",
   fill: false,
@@ -25,13 +37,13 @@ const BandwidthTCPData = {
 
 const BandwidthUDPData = {
   label: "UDP Throughput",
-  data: Bandwidth_UDP.map((entry) => Number(entry.Analysis.ThroughputKbps)),
+  data: BandwidthUDP.map((entry) => Number(entry.Analysis.ThroughputKbps)),
   backgroundColor: "blue",
   borderColor: "blue",
   fill: false,
 };
 
-const Labels = Bandwidth_TCP.map(entry => {
+const Labels = BandwidthTCP.map(entry => {
   const match = entry.File.match(/rate(\d+\.?\d*)/);
   return match ? `${parseInt(match[1]).toLocaleString()} bytes/s` : entry.File;
 });
@@ -39,7 +51,7 @@ const Labels = Bandwidth_TCP.map(entry => {
 
 const PacketLossTCPData = {
   label: "TCP Packet Loss",
-  data: Bandwidth_TCP.map(entry => Number(entry.Analysis.DropRatePercent)),
+  data: BandwidthTCP.map(entry => Number(entry.Analysis.DropRatePercent)),
   backgroundColor: "red",
   borderColor: "red",
   fill: false,
@@ -47,7 +59,7 @@ const PacketLossTCPData = {
 
 const PacketLossUDPData = {
   label: "UDP Packet Loss",
-  data: Bandwidth_UDP.map(entry => Number(entry.Analysis.DropRatePercent)),
+  data: BandwidthUDP.map(entry => Number(entry.Analysis.DropRatePercent)),
   backgroundColor: "orange",
   borderColor: "orange",
   fill: false,
@@ -62,10 +74,37 @@ Header("header", "UDP/TCP Performance in IoT Evaluation", true, [
   { Text: "Citations & References", URL: "#citations" },
 ]);
 
-CreateChart("bandwidth-chart", [BandwidthTCPData, BandwidthUDPData], Labels, "line", "Bandwidth", "Throughput (Kbps)");
-CreateChart("packetloss-chart", [PacketLossTCPData, PacketLossUDPData], Labels, "line", "Bandwidth", "Packet Loss (%)");
-Carousel('carousel', ['Bandwidth', 'Packet Loss'], ["bandwidth-chart", "packetloss-chart"]);
+CreateChart("bandwidth-chart", [BandwidthTCPData, BandwidthUDPData], Labels, "line", "Transmission Rate", "Throughput (Kbps)");
+CreateChart("packetloss-chart", [PacketLossTCPData, PacketLossUDPData], Labels, "line", "Transmission Rate", "Packet Loss (%)");
+Carousel('carousel', ['Throughput', 'Packet Loss'], ["bandwidth-chart", "packetloss-chart"]);
 
+//Upstream & Downstream Charts
 
+const StreamDirectionLabels = ["Upstream TCP", "Upstream UDP", "Downstream TCP", "Downstream UDP"];
+const StreamDirectionThroughputData = {
+  label: "Throughput",
+  data: [
+    UpstreamTCP[0].Analysis.ThroughputKbps,
+    UpstreamUDP[0].Analysis.ThroughputKbps,
+    DownstreamTCP[0].Analysis.ThroughputKbps,
+    DownstreamUDP[0].Analysis.ThroughputKbps],
+  backgroundColor: ["#fc0303", "#fc4a03", "#ad03fc", "#fc03ef"],
+  fill: false,
+}
+
+const StreamDirectionPacketLossData = {
+  label: "Packet Loss",
+  data: [
+    UpstreamTCP[0].Analysis.DropRatePercent,
+    UpstreamUDP[0].Analysis.DropRatePercent,
+    DownstreamTCP[0].Analysis.DropRatePercent,
+    DownstreamUDP[0].Analysis.DropRatePercent],
+  backgroundColor: ["#0314fc", "#03adfc", "#fcad03", "#fcf803"],
+  fill: false,
+}
+
+CreateChart("streamdirection-throughput-chart", [StreamDirectionThroughputData], StreamDirectionLabels, "bar", "Protocol & Direction", "Throughput (Kbps)");
+CreateChart("streamdirection-packetloss-chart", [StreamDirectionPacketLossData], StreamDirectionLabels, "bar", "Protocol & Direction", "Packet Loss (%)");
+Carousel('carousel-streamdirection', ['Throughput', 'Packet Loss'], ["streamdirection-throughput-chart", "streamdirection-packetloss-chart"]);
 
 Carousel('carousel-node-count', ['Throughput', 'Packet Loss'], ["fig_a", "fig_b"]);
